d3a9383cedf22eba41e2d84c6d0d4bc783f75c02
Keith Olson <kolson@dmsi.com>
2022-10-20 10:32:40 -0500

    added comments

--- centralTendancy/out/script.js
+++ centralTendancy/out/script.js
@@ -7,17 +7,19 @@ var clearButton = document.getElementById("clear");
 var numbers = [];
 function addNumber(e) {
     if (!e.target) {
-        return;
+        return; //if e doesn't have a target, none of this makes sense, so shouldn't go through the process. This should never actually happen.
     }
-    e.preventDefault();
+    e.preventDefault(); //Keep submit from refreshing the page
     var li = document.createElement("li");
     var value = e.target["int"].value;
     if (isNaN(value)) {
+        //This should never happen since I'm using a number input, but still protecting for it.
         alert("That is not a number");
+        e.target["int"].value = "";
         return;
     }
     numbers.push(+value);
-    numbers.sort(function (a, b) { return a - b; });
+    numbers.sort(function (a, b) { return a - b; }); //This seems bad. Is there a way to input a number in it's sorted place?
     li.append(value);
     list.append(li);
     updateData();
@@ -48,12 +50,14 @@ function getMedian() {
 function getMode() {
     var maxCount = 0;
     var mode = numbers[0] || 0;
-    var count = numbers.reduce(function (c, n) {
+    numbers.reduce(function (c, n) {
         if (!(n in c)) {
-            c[n] = 0;
+            c[n] = 0; //If n isn't already in c, set it to zero
         }
         c[n]++;
         if (c[n] > maxCount) {
+            //Check if the number of "n" is greater than the max. If it is, it is the new mode.
+            //Note: it's possible to have more than one mode, this will only return one of them.
             maxCount = c[n];
             mode = n;
         }
--- centralTendancy/script.ts
+++ centralTendancy/script.ts
@@ -9,17 +9,22 @@ let numbers: number[] = [];
 
 function addNumber(e: SubmitEvent) {
   if (!e.target) {
-    return;
+    return; //if e doesn't have a target, none of this makes sense, so shouldn't go through the process. This should never actually happen.
   }
-  e.preventDefault();
+  e.preventDefault(); //Keep submit from refreshing the page
+
   const li = document.createElement("li");
   const value = e.target["int"].value;
+
   if (isNaN(value)) {
+    //This should never happen since I'm using a number input, but still protecting for it.
     alert("That is not a number");
+    e.target["int"].value = "";
     return;
   }
+
   numbers.push(+value);
-  numbers.sort((a, b) => a - b);
+  numbers.sort((a, b) => a - b); //This seems bad. Is there a way to input a number in it's sorted place?
   li.append(value);
   list.append(li);
   updateData();
@@ -29,9 +34,7 @@ function addNumber(e: SubmitEvent) {
 
 function updateData() {
   mean.innerHTML = getMean().toFixed(2);
-
   median.innerHTML = getMedian().toString();
-
   mode.innerHTML = getMode().toString();
 }
 
@@ -54,12 +57,14 @@ function getMedian(): number {
 function getMode(): number {
   let maxCount = 0;
   let mode = numbers[0] || 0;
-  const count = numbers.reduce((c, n) => {
+  numbers.reduce((c, n) => {
     if (!(n in c)) {
-      c[n] = 0;
+      c[n] = 0; //If n isn't already in c, set it to zero
     }
     c[n]++;
     if (c[n] > maxCount) {
+      //Check if the number of "n" is greater than the max. If it is, it is the new mode.
+      //Note: it's possible to have more than one mode, this will only return one of them.
       maxCount = c[n];
       mode = n;
     }
