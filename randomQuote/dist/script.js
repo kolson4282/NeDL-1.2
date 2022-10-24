5ceb0b63f21d947f7f3d40b876e930ecfc0c09da
Keith Olson <kolson@dmsi.com>
2022-10-24 11:07:00 -0500

    user form

--- randomQuote/dist/script.js
+++ randomQuote/dist/script.js
@@ -1,3 +1,30 @@
 "use strict";
-console.log("hello");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const contentDiv = document.getElementById("content");
+const authorDiv = document.getElementById("author");
+const form = document.getElementById("userInput");
+const minLengthInput = document.getElementById("minLength");
+const maxLengthInput = document.getElementById("maxLength");
+const displayQuote = (min, max) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield fetch(`https://api.quotable.io/random?minLength=${min}&maxLength=${max}`);
+    const { content, author } = yield result.json();
+    contentDiv.innerText = content;
+    authorDiv.innerText = author;
+});
+const onSubmit = (e) => {
+    e.preventDefault();
+    displayQuote(minLengthInput.value, maxLengthInput.value);
+    minLengthInput.value = "0";
+    maxLengthInput.value = "0";
+    minLengthInput.focus();
+};
+form.addEventListener("submit", onSubmit);
 //# sourceMappingURL=script.js.map
--- randomQuote/script.ts
+++ randomQuote/script.ts
@@ -1,1 +1,39 @@
-console.log("hello");
+const contentDiv = document.getElementById("content") as HTMLDivElement;
+const authorDiv = document.getElementById("author") as HTMLDivElement;
+
+const form = document.getElementById("userInput") as HTMLFormElement;
+const minLengthInput = document.getElementById("minLength") as HTMLInputElement;
+const maxLengthInput = document.getElementById("maxLength") as HTMLInputElement;
+
+type ReturnObject = {
+  _id: string;
+  // The quotation text
+  content: string;
+  // The full name of the author
+  author: string;
+  // The `slug` of the quote author
+  authorSlug: string;
+  // The length of quote (number of characters)
+  length: number;
+  // An array of tag names for this quote
+  tags: string[];
+};
+
+const displayQuote = async (min: string, max: string) => {
+  const result = await fetch(
+    `https://api.quotable.io/random?minLength=${min}&maxLength=${max}`
+  );
+  const { content, author }: ReturnObject = await result.json();
+  contentDiv.innerText = content;
+  authorDiv.innerText = author;
+};
+
+const onSubmit = (e: SubmitEvent) => {
+  e.preventDefault();
+  displayQuote(minLengthInput.value, maxLengthInput.value);
+  minLengthInput.value = "0";
+  maxLengthInput.value = "0";
+  minLengthInput.focus();
+};
+
+form.addEventListener("submit", onSubmit);
