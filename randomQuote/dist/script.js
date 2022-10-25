1727049942182c54eac6ca34ee4e711b44b7787d
Keith Olson <kolson@dmsi.com>
2022-10-25 09:13:50 -0500

    add tag select

--- randomQuote/dist/script.js
+++ randomQuote/dist/script.js
@@ -13,23 +13,49 @@ const authorDiv = document.getElementById("author");
 const form = document.getElementById("userInput");
 const minLengthInput = document.getElementById("minLength");
 const maxLengthInput = document.getElementById("maxLength");
+const tagSelect = document.getElementById("tags");
+const clearButton = document.getElementById("clear");
 const show = document.getElementById("show");
 const json = document.getElementById("json");
-const displayQuote = (min, max) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield fetch(`https://api.quotable.io/random?minLength=${min}&maxLength=${max}`);
+const displayQuote = (min, max, tags = "") => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield fetch(`https://api.quotable.io/random?minLength=${min}&maxLength=${max}&tags=${tags}`);
     const data = yield result.json();
-    contentDiv.innerText = data.content;
-    authorDiv.innerText = data.author;
+    contentDiv.innerText = data.content || data.statusMessage || "";
+    authorDiv.innerText =
+        (data.author ? "- " + data.author : "") +
+            (data.length ? " (Length: " + data.length + ")" : "");
     json.innerText = JSON.stringify(data);
 });
 const onSubmit = (e) => {
     e.preventDefault();
-    displayQuote(minLengthInput.value, maxLengthInput.value);
+    const options = Array.from(tagSelect.options);
+    const selected = [];
+    options.forEach((option) => {
+        if (option.selected) {
+            selected.push(option.value);
+        }
+    });
+    displayQuote(minLengthInput.value, maxLengthInput.value, selected.join(","));
     minLengthInput.focus();
 };
+const fillSubmit = () => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield fetch("https://api.quotable.io/tags");
+    const data = yield result.json();
+    data.forEach((tag) => {
+        const option = document.createElement("option");
+        option.value = tag.name;
+        option.innerText = tag.name;
+        tagSelect.append(option);
+    });
+});
 form.addEventListener("submit", onSubmit);
 show.addEventListener("click", () => {
     json.classList.toggle("hidden");
     show.innerText = show.innerText === "Show JSON" ? "Hide JSON" : "Show JSON";
 });
+clearButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    tagSelect.value = "";
+});
+fillSubmit();
 //# sourceMappingURL=script.js.map
--- randomQuote/script.ts
+++ randomQuote/script.ts
@@ -4,38 +4,73 @@ const authorDiv = document.getElementById("author") as HTMLDivElement;
 const form = document.getElementById("userInput") as HTMLFormElement;
 const minLengthInput = document.getElementById("minLength") as HTMLInputElement;
 const maxLengthInput = document.getElementById("maxLength") as HTMLInputElement;
+const tagSelect = document.getElementById("tags") as HTMLSelectElement;
+const clearButton = document.getElementById("clear") as HTMLButtonElement;
 
 const show = document.getElementById("show") as HTMLButtonElement;
 const json = document.getElementById("json") as HTMLDivElement;
 
 type ReturnObject = {
-  _id: string;
+  _id?: string;
   // The quotation text
-  content: string;
+  content?: string;
   // The full name of the author
-  author: string;
+  author?: string;
   // The `slug` of the quote author
-  authorSlug: string;
+  authorSlug?: string;
   // The length of quote (number of characters)
-  length: number;
+  length?: number;
   // An array of tag names for this quote
-  tags: string[];
+  tags?: string[];
+  statusCode?: number;
+  statusMessage?: string;
 };
 
-const displayQuote = async (min: string, max: string) => {
+type TagObject = {
+  // The number of all tags by this request
+  count: number;
+  // The array of tags
+  results: {
+    _id: string;
+    name: string;
+  }[];
+};
+
+const displayQuote = async (min: string, max: string, tags = "") => {
   const result = await fetch(
-    `https://api.quotable.io/random?minLength=${min}&maxLength=${max}`
+    `https://api.quotable.io/random?minLength=${min}&maxLength=${max}&tags=${tags}`
   );
   const data: ReturnObject = await result.json();
-  contentDiv.innerText = data.content;
-  authorDiv.innerText = data.author;
+
+  contentDiv.innerText = data.content || data.statusMessage || "";
+  authorDiv.innerText =
+    (data.author ? "- " + data.author : "") +
+    (data.length ? " (Length: " + data.length + ")" : "");
   json.innerText = JSON.stringify(data);
 };
 
 const onSubmit = (e: SubmitEvent) => {
   e.preventDefault();
-  displayQuote(minLengthInput.value, maxLengthInput.value);
+  const options = Array.from(tagSelect.options);
+  const selected: string[] = [];
+  options.forEach((option) => {
+    if (option.selected) {
+      selected.push(option.value);
+    }
+  });
+  displayQuote(minLengthInput.value, maxLengthInput.value, selected.join(","));
   minLengthInput.focus();
+};
+
+const fillSubmit = async () => {
+  const result = await fetch("https://api.quotable.io/tags");
+  const data = await result.json();
+  data.forEach((tag: { name: string }) => {
+    const option = document.createElement("option");
+    option.value = tag.name;
+    option.innerText = tag.name;
+    tagSelect.append(option);
+  });
 };
 
 form.addEventListener("submit", onSubmit);
@@ -43,3 +78,8 @@ show.addEventListener("click", () => {
   json.classList.toggle("hidden");
   show.innerText = show.innerText === "Show JSON" ? "Hide JSON" : "Show JSON";
 });
+clearButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  tagSelect.value = "";
+});
+fillSubmit();
