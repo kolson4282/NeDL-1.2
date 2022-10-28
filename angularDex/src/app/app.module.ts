54ef35cba029778ef5d46004febfa149aa4f5119
Keith Olson <kolson@dmsi.com>
2022-10-28 13:58:53 -0500

    initial commit

---
+++ angularDex/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
