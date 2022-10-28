03450e9c3bca325d0eb202cd7b5fff341c31fbe9
Keith Olson <kolson@dmsi.com>
2022-10-28 12:17:35 -0500

    get pokemon list from API

--- pokedex/dist/script.js
+++ pokedex/dist/script.js
@@ -1,74 +1,58 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const mainContent = document.getElementById("mainContent");
-const bulbasaur = {
-    pokemon: {
-        sprites: {
-            other: {
-                "official-artwork": {
-                    front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-                },
-            },
-        },
-        types: [
-            {
-                slot: 1,
-                type: {
-                    name: "grass",
-                    url: "",
-                },
-            },
-        ],
-    },
-    pokemonSpecies: {
-        name: "bulbasaur",
-        id: 1,
-        genera: [
-            {
-                genus: "たねポケモン",
-                language: {
-                    name: "ja",
-                    url: "",
-                },
-            },
-            {
-                genus: "Seed Pokemon",
-                language: {
-                    name: "en",
-                    url: "string",
-                },
-            },
-        ],
-    },
-};
-const load = () => {
-    for (let i = 0; i < 12; i++) {
-        mainContent.innerHTML += pokemonCard(bulbasaur);
-    }
-};
+const load = () => __awaiter(void 0, void 0, void 0, function* () {
+    const pokemonListResult = yield fetch("https://pokeapi.co/api/v2/pokemon-species");
+    const pokemonList = yield pokemonListResult.json();
+    yield getCompositePokemon(pokemonList).then((list) => list.forEach((p) => (mainContent.innerHTML += pokemonCard(p))));
+    // compPokemonList.forEach((p) => (mainContent.innerHTML += pokemonCard(p)));
+    // console.log(compPokemonList);
+});
+const getCompositePokemon = (list) => __awaiter(void 0, void 0, void 0, function* () {
+    const promises = list.results.map((p) => __awaiter(void 0, void 0, void 0, function* () {
+        const pokemonSpecies = yield fetch(p.url).then((r) => r.json());
+        const pokemon = yield fetch(pokemonSpecies.varieties[0].pokemon.url).then((r) => r.json());
+        return { pokemon, pokemonSpecies };
+    }));
+    const compPokemonList = yield Promise.all(promises).then((list) => list.sort((a, b) => a.pokemonSpecies.id - b.pokemonSpecies.id));
+    return compPokemonList;
+});
 const pokemonCard = ({ pokemon, pokemonSpecies }) => {
     const genera = pokemonSpecies.genera.find((gen) => gen.language.name === "en");
-    return `<div class="col-xs-6 col-md-4 col-lg-3 col-xxl-2">
-<div class="card">
-  <img
-    src="${pokemon.sprites.other["official-artwork"].front_default}"
-    alt="${pokemonSpecies.name}"
-    class="card-img-top"
-  />
-  <div class="card-body">
-    <div class="card-title">${pokemonSpecies.name}</div>
-    ${genera &&
-        `<div class="card-subtitle mv-2 text-muted">${genera.genus}</div>`}
-  </div>
-  <ul
-    class="list-group list-group-horizontal justify-content-center"
-    style="width: 100%"
-  >${pokemon.types
-        .map((type) => `<l1 class="list-group-item .flex-fill" style="width: 100%"
-        >${type.type.name}</l1
-      >`)
+    return `
+  <div class="col-xs-6 col-md-4 col-lg-3 col-xxl-2">
+    <div class="card" style="height: 100%">
+      <img
+        src="${pokemon.sprites.other["official-artwork"].front_default}"
+        alt="${pokemonSpecies.name}"
+        class="card-img-top"
+      />
+      <div class="card-body">
+        <div class="card-title">
+          ${pokemonSpecies.id} - ${pokemonSpecies.name}
+        </div>
+        ${genera &&
+        `<div class="card-subtitle mv-2 text-muted">${genera.genus}</div>`}     
+      </div>
+      <ul
+        class="list-group list-group-horizontal justify-content-center"
+        style="width: 100%">
+        ${pokemon.types
+        .map((type) => `<l1 class="list-group-item .flex-fill" style="width: 100%">
+                ${type.type.name}
+                </l1>`)
         .join("")}
-  </ul>
-</div>
-</div>`;
+      </ul>
+    </div>
+  </div>
+  `;
 };
 load();
--- pokedex/script.ts
+++ pokedex/script.ts
@@ -1,5 +1,15 @@
 const mainContent = document.getElementById("mainContent") as HTMLElement;
 
+type PokemonSpeciesList = {
+  count: number;
+  next: string;
+  previous: string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+};
+
 type Pokemon = {
   sprites: {
     other: {
@@ -27,6 +37,13 @@ type PokemonSpecies = {
       url: string;
     };
   }[];
+  varieties: {
+    is_default: boolean;
+    pokemon: {
+      name: string;
+      url: string;
+    };
+  }[];
 };
 
 type CompositePokemon = {
@@ -34,53 +51,32 @@ type CompositePokemon = {
   pokemonSpecies: PokemonSpecies;
 };
 
-const bulbasaur: CompositePokemon = {
-  pokemon: {
-    sprites: {
-      other: {
-        "official-artwork": {
-          front_default:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-      },
-    },
-    types: [
-      {
-        slot: 1,
-        type: {
-          name: "grass",
-          url: "",
-        },
-      },
-    ],
-  },
-
-  pokemonSpecies: {
-    name: "bulbasaur",
-    id: 1,
-    genera: [
-      {
-        genus: "たねポケモン",
-        language: {
-          name: "ja",
-          url: "",
-        },
-      },
-      {
-        genus: "Seed Pokemon",
-        language: {
-          name: "en",
-          url: "string",
-        },
-      },
-    ],
-  },
+const load = async () => {
+  const pokemonListResult = await fetch(
+    "https://pokeapi.co/api/v2/pokemon-species"
+  );
+  const pokemonList: PokemonSpeciesList = await pokemonListResult.json();
+  await getCompositePokemon(pokemonList).then((list) =>
+    list.forEach((p) => (mainContent.innerHTML += pokemonCard(p)))
+  );
+  // compPokemonList.forEach((p) => (mainContent.innerHTML += pokemonCard(p)));
+  // console.log(compPokemonList);
 };
 
-const load = () => {
-  for (let i = 0; i < 12; i++) {
-    mainContent.innerHTML += pokemonCard(bulbasaur);
-  }
+const getCompositePokemon = async (list: PokemonSpeciesList) => {
+  const promises = list.results.map(async (p) => {
+    const pokemonSpecies: PokemonSpecies = await fetch(p.url).then((r) =>
+      r.json()
+    );
+    const pokemon: Pokemon = await fetch(
+      pokemonSpecies.varieties[0].pokemon.url
+    ).then((r) => r.json());
+    return { pokemon, pokemonSpecies };
+  });
+  const compPokemonList: CompositePokemon[] = await Promise.all(promises).then(
+    (list) => list.sort((a, b) => a.pokemonSpecies.id - b.pokemonSpecies.id)
+  );
+  return compPokemonList;
 };
 
 const pokemonCard = ({ pokemon, pokemonSpecies }: CompositePokemon): string => {
@@ -88,34 +84,38 @@ const pokemonCard = ({ pokemon, pokemonSpecies }: CompositePokemon): string => {
     (gen) => gen.language.name === "en"
   );
 
-  return `<div class="col-xs-6 col-md-4 col-lg-3 col-xxl-2">
-<div class="card">
-  <img
-    src="${pokemon.sprites.other["official-artwork"].front_default}"
-    alt="${pokemonSpecies.name}"
-    class="card-img-top"
-  />
-  <div class="card-body">
-    <div class="card-title">${pokemonSpecies.name}</div>
-    ${
-      genera &&
-      `<div class="card-subtitle mv-2 text-muted">${genera.genus}</div>`
-    }
+  return `
+  <div class="col-xs-6 col-md-4 col-lg-3 col-xxl-2">
+    <div class="card" style="height: 100%">
+      <img
+        src="${pokemon.sprites.other["official-artwork"].front_default}"
+        alt="${pokemonSpecies.name}"
+        class="card-img-top"
+      />
+      <div class="card-body">
+        <div class="card-title">
+          ${pokemonSpecies.id} - ${pokemonSpecies.name}
+        </div>
+        ${
+          genera &&
+          `<div class="card-subtitle mv-2 text-muted">${genera.genus}</div>`
+        }     
+      </div>
+      <ul
+        class="list-group list-group-horizontal justify-content-center"
+        style="width: 100%">
+        ${pokemon.types
+          .map(
+            (type) =>
+              `<l1 class="list-group-item .flex-fill" style="width: 100%">
+                ${type.type.name}
+                </l1>`
+          )
+          .join("")}
+      </ul>
+    </div>
   </div>
-  <ul
-    class="list-group list-group-horizontal justify-content-center"
-    style="width: 100%"
-  >${pokemon.types
-    .map(
-      (type) =>
-        `<l1 class="list-group-item .flex-fill" style="width: 100%"
-        >${type.type.name}</l1
-      >`
-    )
-    .join("")}
-  </ul>
-</div>
-</div>`;
+  `;
 };
 
 load();
